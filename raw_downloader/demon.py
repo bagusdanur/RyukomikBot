1f2a144f44b9011e722da24483118f4936143cc7
bagusdanur <kanimenia778@gmail.com>
2026-08-11 15:30:06 +0700

    Resolve Demon chapter numbers to official slugs

--- raw_downloader/demon.py
+++ raw_downloader/demon.py
@@ -1,5 +1,6 @@
 from .qimanga import QiMangaDownloader
 from config import DEMON_API
+from chapter_utils import normalize_chapter
 
 class DemonDownloader(QiMangaDownloader):
     def __init__(self): self.api_url = DEMON_API.rstrip("/")
@@ -14,4 +15,15 @@ class DemonDownloader(QiMangaDownloader):
         rows = await super().get_chapter_list(str(manga_id).removeprefix("manga/"))
         for row in rows: row["source"] = "demon"
         return rows
-    async def get_chapter_images(self, manga_id, chapter_id): return await super().get_chapter_images(str(manga_id).removeprefix("manga/"), chapter_id)
+    async def get_chapter_images(self, manga_id, chapter_id):
+        manga_id = str(manga_id).removeprefix("manga/")
+        wanted = normalize_chapter(str(chapter_id))
+        if wanted:
+            chapters = await self.get_chapter_list(manga_id)
+            matched = next((item for item in chapters if wanted in {
+                normalize_chapter(str(item.get("id", ""))),
+                normalize_chapter(str(item.get("title", ""))),
+            }), None)
+            if matched:
+                chapter_id = matched["id"]
+        return await super().get_chapter_images(manga_id, chapter_id)
